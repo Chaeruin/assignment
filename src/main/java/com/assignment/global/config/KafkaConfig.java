e5c76a62b342eff00b64aa9ed228a6148b2bc4f2
Chaeruin <cms3604@naver.com>
2026-05-06 14:22:28 +0900

    feat: add Kafka retry scheduler bean in Config

--- src/main/java/com/assignment/global/config/KafkaConfig.java
+++ src/main/java/com/assignment/global/config/KafkaConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableKafkaRetryTopic
@@ -13,8 +15,8 @@ public class KafkaConfig {
     @Bean
     public NewTopic couponIssueTopic() {
         return TopicBuilder.name("coupon-issue-topic")
-                .partitions(12)
-                .replicas(3)
+                .partitions(3)
+                .replicas(1)
                 .build();
     }
 
@@ -23,7 +25,16 @@ public class KafkaConfig {
     public NewTopic couponIssueDltTopic() {
         return TopicBuilder.name("coupon-issue-topic-dlt")
                 .partitions(1)
-                .replicas(3)
+                .replicas(1)
                 .build();
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("kafka-retry-scheduler-");
+        scheduler.initialize();
+        return scheduler;
     }
 }

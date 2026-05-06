a571941be535a7c9b4c729b6a5add5a983f6f897
Chaeruin <cms3604@naver.com>
2026-05-06 13:23:28 +0900

    fix: modify exception handler class and common response part

--- src/main/java/com/assignment/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/assignment/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.assignment.global.exception;
 
 import com.assignment.global.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,19 +14,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CouponException.class)
     public ResponseEntity<ApiResponse<Void>> handleCouponException(CouponException e) {
         log.info(e.getMessage(), e);
-        ApiResponse<Void> response = ApiResponse.fail(
-                e.getMessage()
-        );
-        return ResponseEntity.status(e.getHttpStatus()).body(response);
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(ApiResponse.fail(e.getMessage()));
     }
 
-    @ExceptionHandler(CouponException.class)
+    @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ApiResponse<Void>> handleAuthorizedException(UnauthorizedException e) {
         log.info(e.getMessage(), e);
-        ApiResponse<Void> response = ApiResponse.fail(
-                e.getMessage()
-        );
-        return ResponseEntity.status(e.getHttpStatus()).body(response);
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(ApiResponse.fail(e.getMessage()));
     }
 }
 
